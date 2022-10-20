5f0796da2c325a38a1b86aebe907b10ef90c95a6
matheuszack <113534912+matheuszack@users.noreply.github.com>
2022-10-20 20:37:09 -0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,46 +1,46 @@
 const selecione = window.document.querySelector(".select");
 const escolha = window.document.querySelector(".chave-escolha");
-const radiobtn = window.document.querySelector(".radio-button");
-const btn = window.document.querySelector("button");
+const buradio = window.document.querySelector(".radio-button");
+const botao = window.document.querySelector("button");
 const codificar = window.document.querySelector("#codificar");
 const decodificar = window.document.querySelector("#decodificar");
 //utilizando o const, pois ele não pode ser redefinido, assim mantendo seu valor de atribuição fixo.
 
 
-
+ 
 selecione.addEventListener("click", function () {
-    if (selecione.value == "cifra") {
-      escolha.style.display = "block";
-    } else {
-      escolha.style.display = "none";
-    }
-  });
+  if (selecione.value == "base64") {
+    escolha.style.display = "none";
+  } else {
+    if (selecione.value=='cifra') {
+    escolha.style.display = "block";}
+  }
+});
 //evento simples para a modificação da chave de escolha.
-  function base64() {
-    let mensagem = document.querySelector("#mensagem").value;
   
+  function base64() {
+    var texto = document.querySelector("#mensagem").value;
     if (codificar.checked) {
-      let codificado = window.btoa(mensagem);
+      var codificado = window.btoa(texto);
       return codificado;
     } else if (decodificar.checked) {
-      let decodificado = window.atob(mensagem);
+      var decodificado = window.atob(texto);
       return decodificado;
     }
   }//configuração  da base64 quando adicionado a mensagem.
 
- 
-  
+
   function cifraCesar() {
-    let msg = document.querySelector("#mensagem").value;
-    let chave = parseInt(document.querySelector("#rangenumber").value);
-    let saida = '';
+    var msg = document.querySelector("#mensagem").value;
+    var numero = parseInt(document.querySelector("#rangenumber").value);
+    var saida = '';
   
     if (codificar.checked) {
       for (let i = 0; i < msg.length; i++) {
         if (msg[i] === msg[i].toUpperCase()) {
-          saida += String.fromCharCode((msg.charCodeAt(i) + chave - 65) % 26 + 65); 
+          saida += String.fromCharCode((msg.charCodeAt(i) + numero - 65) % 26 + 65); 
         } else {
-          saida += String.fromCharCode((msg.charCodeAt(i) + chave - 97) % 26 + 97);
+          saida += String.fromCharCode((msg.charCodeAt(i) + numero - 97) % 26 + 97);
         }
       }
       return saida;
@@ -48,9 +48,9 @@ selecione.addEventListener("click", function () {
     } else if (decodificar.checked) {
       for (let i = 0; i < msg.length; i++) {
         if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
-          saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
+          saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  numero + 26) % 26 + 97);
         } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
-          saida += String.fromCharCode((msg.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
+          saida += String.fromCharCode((msg.charCodeAt(i) - 65 - numero + 26) % 26 + 65);
         } else {
           saida += String.fromCharCode(msg.charCodeAt(i));
         }
@@ -58,15 +58,15 @@ selecione.addEventListener("click", function () {
       return saida;
     }
   }
-  radiobtn.addEventListener("click", function () {
+ 
+  buradio.addEventListener("click", function () {
     if (codificar.checked) {
-      btn.innerHTML = "<strong>Codificar Mensagem!</strong>";
+      botao.innerHTML = "<strong>Codificar Mensagem!</strong>";
     } else if (decodificar.checked) {
-      btn.innerHTML = "<strong>Decodificar Mensagem!</strong>";
+      botao.innerHTML = "<strong>Decodificar Mensagem!</strong>";
     }
   });
-  
-  btn.addEventListener("click", function (event) {
+  botao.addEventListener("click", function (event) {
     event.preventDefault();
     if (selecione.value === "base64") {
       resultado.innerText = base64();
@@ -74,5 +74,6 @@ selecione.addEventListener("click", function () {
       resultado.innerText = cifraCesar();
     }
   });
+  
   
   
